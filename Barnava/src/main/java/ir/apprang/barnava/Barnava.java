4c5f6b4718be99ca75514aadff4e4fac26805591
mahdi.farzami <mahdi.farzami@gmail.com>
2020-12-15 00:31:22 +0330

    complete sample app fix colored status bar bug

--- Barnava/src/main/java/ir/apprang/barnava/Barnava.java
+++ Barnava/src/main/java/ir/apprang/barnava/Barnava.java
@@ -132,8 +132,8 @@ public class Barnava {
                 window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
                 if (light_statusbar)
                     window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
-                else
-                    window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+                //else
+                //    window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
                 window.setStatusBarColor(ContextCompat.getColor(activity, statusBarColor));
                 window.setNavigationBarColor(ContextCompat.getColor(activity, navigationBarColor));
             }
@@ -180,9 +180,9 @@ public class Barnava {
                 window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
                 window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
                 if (light_statusbar)
-                    window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
-                else
-                    window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+                    window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
+                //else
+                //window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
                 window.setStatusBarColor(ContextCompat.getColor(activity, statusBarColor));
             }
 
--- app/src/main/java/ir/apprang/barnavasample/MainActivity.java
+++ app/src/main/java/ir/apprang/barnavasample/MainActivity.java
@@ -6,10 +6,21 @@ import android.view.View;
 import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
 import ir.apprang.barnava.Barnava;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    Button btn_statusbar_do_no_thing;
+    Button btn_statusbar_set_color;
+    Button btn_statusbar_transparent;
+    Button btn_statusbar_light;
+    Button btn_statusbar_dark;
+    Button btn_navigation_do_no_thing;
+    Button btn_navigation_set_color;
+    Button btn_navigation_transparent;
 
     int statusbar = 0;
     int navigationbar = 0;
@@ -26,6 +37,7 @@ public class MainActivity extends AppCompatActivity {
 
 
         if (getIntent().hasExtra("statusbar")) {
+
             statusbar = getIntent().getIntExtra("statusbar", 0);
             navigationbar = getIntent().getIntExtra("navigationbar", 0);
             light_mode = getIntent().getBooleanExtra("light_mode", false);
@@ -34,7 +46,7 @@ public class MainActivity extends AppCompatActivity {
         }
 
 
-        Button btn_statusbar_do_no_thing = findViewById(R.id.doNoThing);
+        btn_statusbar_do_no_thing = findViewById(R.id.doNoThing);
         btn_statusbar_do_no_thing.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -44,7 +56,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        Button btn_statusbar_set_color = findViewById(R.id.colored);
+        btn_statusbar_set_color = findViewById(R.id.colored);
         btn_statusbar_set_color.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -53,7 +65,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        Button btn_statusbar_transparent = findViewById(R.id.transparent);
+        btn_statusbar_transparent = findViewById(R.id.transparent);
         btn_statusbar_transparent.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -62,7 +74,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        Button btn_statusbar_light = findViewById(R.id.light);
+        btn_statusbar_light = findViewById(R.id.light);
         btn_statusbar_light.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -71,7 +83,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        Button btn_statusbar_dark = findViewById(R.id.dark);
+        btn_statusbar_dark = findViewById(R.id.dark);
         btn_statusbar_dark.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -80,7 +92,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        Button btn_navigation_do_no_thing = findViewById(R.id.NdoNoThing);
+        btn_navigation_do_no_thing = findViewById(R.id.NdoNoThing);
         btn_navigation_do_no_thing.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -89,7 +101,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        Button btn_navigation_set_color = findViewById(R.id.Ncolored);
+        btn_navigation_set_color = findViewById(R.id.Ncolored);
         btn_navigation_set_color.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -98,7 +110,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        Button btn_navigation_transparent = findViewById(R.id.Ntransparent);
+        btn_navigation_transparent = findViewById(R.id.Ntransparent);
         btn_navigation_transparent.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -107,6 +119,8 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+
+        changeBtnColor();
 
     }
 
@@ -118,6 +132,50 @@ public class MainActivity extends AppCompatActivity {
         intent.putExtra("light_mode", light_mode);
         startActivity(intent);
         finish();
+
+    }
+
+
+    public void changeBtnColor() {
+
+        if (statusbar > 0) {
+            btn_statusbar_do_no_thing.setBackgroundColor(ContextCompat.getColor(MainActivity.this, R.color.green));
+            btn_statusbar_set_color.setBackgroundColor(ContextCompat.getColor(MainActivity.this, R.color.red));
+            btn_statusbar_transparent.setBackgroundColor(ContextCompat.getColor(MainActivity.this, R.color.green));
+        } else if (statusbar < 0) {
+            btn_statusbar_do_no_thing.setBackgroundColor(ContextCompat.getColor(MainActivity.this, R.color.green));
+            btn_statusbar_set_color.setBackgroundColor(ContextCompat.getColor(MainActivity.this, R.color.green));
+            btn_statusbar_transparent.setBackgroundColor(ContextCompat.getColor(MainActivity.this, R.color.red));
+        } else if (statusbar == 0) {
+            btn_statusbar_do_no_thing.setBackgroundColor(ContextCompat.getColor(MainActivity.this, R.color.red));
+            btn_statusbar_set_color.setBackgroundColor(ContextCompat.getColor(MainActivity.this, R.color.green));
+            btn_statusbar_transparent.setBackgroundColor(ContextCompat.getColor(MainActivity.this, R.color.green));
+        }
+
+
+        if (light_mode) {
+            btn_statusbar_light.setBackgroundColor(ContextCompat.getColor(MainActivity.this, R.color.red));
+            btn_statusbar_dark.setBackgroundColor(ContextCompat.getColor(MainActivity.this, R.color.green));
+        } else {
+            btn_statusbar_light.setBackgroundColor(ContextCompat.getColor(MainActivity.this, R.color.green));
+            btn_statusbar_dark.setBackgroundColor(ContextCompat.getColor(MainActivity.this, R.color.red));
+        }
+
+
+        if (navigationbar > 0) {
+            btn_navigation_do_no_thing.setBackgroundColor(ContextCompat.getColor(MainActivity.this, R.color.green));
+            btn_navigation_set_color.setBackgroundColor(ContextCompat.getColor(MainActivity.this, R.color.red));
+            btn_navigation_transparent.setBackgroundColor(ContextCompat.getColor(MainActivity.this, R.color.green));
+        } else if (navigationbar < 0) {
+            btn_navigation_do_no_thing.setBackgroundColor(ContextCompat.getColor(MainActivity.this, R.color.green));
+            btn_navigation_set_color.setBackgroundColor(ContextCompat.getColor(MainActivity.this, R.color.green));
+            btn_navigation_transparent.setBackgroundColor(ContextCompat.getColor(MainActivity.this, R.color.red));
+        } else if (navigationbar == 0) {
+            btn_navigation_do_no_thing.setBackgroundColor(ContextCompat.getColor(MainActivity.this, R.color.red));
+            btn_navigation_set_color.setBackgroundColor(ContextCompat.getColor(MainActivity.this, R.color.green));
+            btn_navigation_transparent.setBackgroundColor(ContextCompat.getColor(MainActivity.this, R.color.green));
+        }
+
 
     }
 
